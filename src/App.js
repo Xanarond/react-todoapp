0d52210d99921bdf892c09951418d82c2c09d3a4
Vlad <vlakos2895@gmail.com>
2020-08-24 20:28:52 +0300

    Config app
    Add eslint config
    Redux prepare config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Provider } from 'react-redux';
+import store from './store/store';
+import { TodoList } from './components/TodoList';
 
-export default App;
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoList />
+  </Provider>,
+  rootElement,
+);
