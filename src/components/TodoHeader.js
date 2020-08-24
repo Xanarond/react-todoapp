4381611145e3e09b5a8479dafb5af01680cb3ff3
Vlad <vlakos2895@gmail.com>
2020-08-25 00:17:06 +0300

    dev

---
+++ src/components/TodoHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TodoHeader(props) {
+  return (
+    <div className="todo-header">
+      <h1>Todo List</h1>
+    </div>
+  );
+}
+
+export default TodoHeader;
