4aaef1ddc3060a19de5cc143a6978eabb7597028
Vlad <vlakos2895@gmail.com>
2020-09-01 09:42:01 +0300

    Add simple validation form

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -10,18 +10,33 @@ class InputForm extends Component {
             content: ''
         };
         this.handleAddTodo = this.handleAddTodo.bind(this);
+        this.validateTitle = this.validateTitle.bind(this);
+        this.validateText = this.validateText.bind(this)
     }
 
+    validateTitle(label) {
+        return label.length > 6;
+    }
+
+    validateText(content) {
+        return content.length > 3;
+    }
 
     onLabelChange = (e) => {
+        let val = e.target.value
+        let valid = this.validateTitle(val);
         this.setState({
-            label: e.target.value
+            label: e.target.value,
+            labelValid: valid
         })
     };
 
     onContentChange = (e) => {
+        let val = e.target.value
+        let valid = this.validateText(val);
         this.setState({
-            content: e.target.value
+            content: e.target.value,
+            contentValid: valid
         })
     };
 
@@ -36,9 +51,14 @@ class InputForm extends Component {
 
     handleAddTodo() {
         this.props.addTodo(this.state.label, this.state.content);
+        this.setState({contentValid: false, labelValid: false,});
     }
 
     render() {
+        // цвет границы для поля для ввода имени
+        var titleColor = this.state.labelValid === true ? "green" : "#3d7ec0";
+        // цвет границы для поля для ввода возраста
+        var contentColor = this.state.contentValid === true ? "green" : "#3d7ec0";
         return (
             <div className="todo-input">
                 <form
@@ -52,6 +72,7 @@ class InputForm extends Component {
                         onChange={this.onLabelChange}
                         value={this.state.label}
                         placeholder="Title"
+                        style={{borderColor: titleColor}}
                     />
 
                     <input
@@ -60,11 +81,13 @@ class InputForm extends Component {
                         onChange={this.onContentChange}
                         value={this.state.content}
                         placeholder="Content"
+                        style={{borderColor: contentColor}}
                     />
 
                     <button
                         type="submit"
-                        className="btn btn-outline-secondary"
+                        disabled={!this.state.labelValid || !this.state.contentValid}
+                        className="btn"
                         onClick={this.handleAddTodo}
                     >
                         Add
